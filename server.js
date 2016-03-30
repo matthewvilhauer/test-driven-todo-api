65c84f18bb191cdeaecf51402a832354c76a60d6
Matthew Vilhauer <matthewvilhauer@gmail.com>
2016-03-29 20:24:01 -0700

    WOrked on implementing some of these requests with LoDash for fun

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 var express = require('express'),
     app = express(),
     bodyParser = require('body-parser');
+var _ = require('lodash');
 
 // configure bodyParser (for receiving form data)
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -15,9 +16,9 @@ app.use(express.static(__dirname + '/public'));
 
 // our database is an array for now with some hardcoded values
 var todos = [
-  // { _id: 1, task: 'Laundry', description: 'Wash clothes' },
-  // { _id: 2, task: 'Grocery Shopping', description: 'Buy dinner for this week' },
-  // { _id: 3, task: 'Homework', description: 'Make this app super awesome!' }
+  { _id: 5, task: 'Laundry', description: 'Wash clothes' },
+  { _id: 7, task: 'Grocery Shopping', description: 'Buy dinner for this week' },
+  { _id: 9, task: 'Homework', description: 'Make this app super awesome!' }
 ];
 
 /**********
@@ -52,18 +53,53 @@ app.get('/api/todos/search', function search(req, res) {
 app.get('/api/todos', function index(req, res) {
   /* This endpoint responds with all of the todos
    */
+   res.json({todos: todos});
 });
 
 app.post('/api/todos', function create(req, res) {
   /* This endpoint will add a todo to our "database"
    * and respond with the newly created todo.
    */
+   var id = todos.length + 1;
+   var task = req.body.task;
+   var descr = req.body.description;
+   var newToDo = {_id: id, task: task, description: descr};
+   console.log(newToDo);
+   todos.push(newToDo);
+   res.json(newToDo);
+
 });
 
 app.get('/api/todos/:id', function show(req, res) {
   /* This endpoint will return a single todo with the
    * id specified in the route parameter (:id)
    */
+  //  var id = todos.length + 1;
+   var id = parseInt(req.params.id);
+   var aTodo = [];
+   //downside to forEach on server is that it doesnt have a way to break...
+   // case/switch statments can break
+   // so can for() loops
+   todos.forEach(function(todo, i){
+      if (todo._id == id){
+        console.log('todo', i, todo._id);
+        aTodo.push(todo);
+      }
+   })
+   res.json(aTodo);
+
+    // alternative
+    var foundTodo = _.find(todos, function(todo){
+        return todo._id === req.params.id;
+    });
+
+    var otherTodos = _.filter(todos, function(t){
+        return t._id !== req.params.id;
+    })
+
+    // var ft = _find(todos, (todo) => todo._id === req.params.id);
+    res.json(foundTodo);
+
 });
 
 app.put('/api/todos/:id', function update(req, res) {
@@ -71,13 +107,44 @@ app.put('/api/todos/:id', function update(req, res) {
    * id specified in the route parameter (:id) and respond
    * with the newly updated todo.
    */
+   var id = parseInt(req.body._id);
+   //grab prevTodo from todos []
+   var prevTodo = _.remove(todos, function(t) {
+     return t._id === id;
+   });
+   //make a new object by extending {} with prevTodo and newTodo
+   var newTodo = {
+       _id: req.params.id,
+       description: req.body.description,
+       task: req.body.task
+   };
+   var updatedTodo =  _.extend({}, prevTodo, newTodo);
+   todos.push(updatedTodo);
+
+    res.json(updatedTodo);
+   // req.send(todos[req.params._id]);
+   // res.send(todos[req.params._id]);
+
 });
+function findTodos(id) {
+
+}
 
 app.delete('/api/todos/:id', function destroy(req, res) {
   /* This endpoint will delete a single todo with the
    * id specified in the route parameter (:id) and respond
    * with success.
    */
+   var id = parseInt(req.params.id);
+   todos.forEach(function(todo){
+
+   })
+   var newTodos = filter(function(f){
+     return f._id !== id;
+   })
+   res.json(newTodos);
+
+  //  req.delete(todos[id-1]);
 });
 
 /**********
